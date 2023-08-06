895338c3411d71f367d5d6df9deb053f101a61a7
Kelvin-Yeung <y.kelvin2004@gmail.com>
2023-08-06 02:08:39 -0400

    Added resizability functionality and improved webpage structure

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 const board = document.querySelector(".container");
+const changeSize = document.querySelector("button");
 
 function createGrid(size) {
   for (let i = 0; i < size * size; ++i) {
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
     const square = document.createElement("div");
     square.classList.add("element");
     board.appendChild(square);
+    board.addEventListener("mouseover", addFilled);
+  }
+}
+
+function removeGrid() {
+  while (board.firstChild) {
+    board.removeChild(board.firstChild);
   }
 }
 
@@ -12,8 +22,10 @@ function addFilled(event) {
   event.target.classList.add("filled");
 }
 
-createGrid(16);
-const grid = document.querySelectorAll(".element");
-for (const block of grid) {
-  block.addEventListener("mouseenter", addFilled);
-}
+changeSize.addEventListener("click", () => {
+  let size = prompt("What size do you want the grid to be?", 16);
+  removeGrid()
+  createGrid(size);
+});
+
+window.onload = createGrid(16);
