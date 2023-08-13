cdb877dbbf7bca4c44b46e2b4523bd7bb6b9cff6
Kelvin-Yeung <y.kelvin2004@gmail.com>
2023-08-13 19:35:46 -0400

    Added colour change functionality of common RGB values

--- script.js
+++ script.js
@@ -2,8 +2,11 @@ const MIN_SIZE = 1;
 const MAX_SIZE = 50;
 const DEFAULT_SIZE = 16;
 const canvas = document.querySelector(".container");
+const lightBtns = document.querySelector(".lightshades");
+const darkBtns = document.querySelector(".darkshades");
 const sizeButton = document.querySelector("button");
 let drawing = false;
+let colour = "blue";
 
 function createCanvas(size) {
   if (size < MIN_SIZE || size > MAX_SIZE) {
@@ -16,9 +19,63 @@ function createCanvas(size) {
     const square = document.createElement("div");
     square.classList.add("pixel");
     canvas.appendChild(square);
+    canvas.addEventListener("mousedown", paintCanvas);
     canvas.addEventListener("mouseover", paintCanvas);
   }
 }
+
+function createColourBtns() {
+  // Learn how to error handle in JS
+  let rgbvals = [255, 255, 255, 128, 0, 0, 0, 0, 0, 128, 255, 255];
+  for (let i = 0; i < 12; ++i) { 
+    for (let j = 0; j < 5; ++j) {
+      let lightRed = rgbvals[i] + (j * 51);
+      let lightGreen = rgbvals[(i + 8) % 12] + (j * 51);
+      let lightBlue = rgbvals[(i + 4) % 12] + (j * 51);
+      const btn = document.createElement("button");
+      btn.style.type = "button";
+      btn.style.width = "30px";
+      btn.style.height = "30px";
+      btn.style.borderRadius = "50%";
+      btn.style.backgroundColor = "rgb(" + Math.min(lightRed, 255) + ',' + Math.min(lightGreen, 255) + ',' + Math.min(lightBlue, 255) + ')';
+      btn.addEventListener("click", (event) => {
+        colour = event.target.style.backgroundColor;
+      });
+      lightBtns.appendChild(btn);
+      console.log("Red: " + lightRed);
+      console.log("Blue: " + lightBlue);
+      console.log("Green: " + lightGreen);
+    }
+  }
+}
+
+function createColourBtns2() {
+  // Learn how to error handle in JS
+  let rgbvals = [255, 255, 255, 128, 0, 0, 0, 0, 0, 128, 255, 255];
+  for (let i = 0; i < 12; ++i) { 
+    for (let j = 4; j > 0; --j) {
+      let red = rgbvals[i] - (j * 51);
+      let green = rgbvals[(i + 8) % 12] - (j * 51);
+      let blue = rgbvals[(i + 4) % 12] - (j * 51);
+      const btn = document.createElement("button");
+      btn.style.type = "button";
+      btn.style.width = "30px";
+      btn.style.height = "30px";
+      btn.style.borderRadius = "50%";
+      btn.style.backgroundColor = "rgb(" + Math.max(red, 0) + ',' + Math.max(green, 0) + ',' + Math.max(blue, 0) + ')';
+      btn.addEventListener("click", (event) => {
+        colour = event.target.style.backgroundColor;
+      });
+      darkBtns.appendChild(btn);
+      console.log("Red: " + red);
+      console.log("Blue: " + green);
+      console.log("Green: " + blue);
+    }
+  }
+}
+
+createColourBtns();
+createColourBtns2();
 
 function destroyCanvas() {
   while (canvas.firstChild) {
@@ -27,8 +84,8 @@ function destroyCanvas() {
 }
 
 function paintCanvas(event) {
-  if (drawing) {
-    event.target.style.backgroundColor = "blue";
+  if (drawing || event.type === "mousedown") {
+    event.target.style.backgroundColor = colour;
   }
 }
 
